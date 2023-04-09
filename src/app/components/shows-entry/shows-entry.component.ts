29e07a38de04c981817f15f5d0b2e851bacf32c3
Jeff Gonzalez <jeff@hypertheory.com>
2023-04-09 11:19:19 -0400

    form validation

--- src/app/components/shows-entry/shows-entry.component.ts
+++ src/app/components/shows-entry/shows-entry.component.ts
@@ -1,46 +1,70 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ShowsDataService, selectStreamingPlatforms } from 'src/app/shows-data.service';
 import { ShowCreate } from 'src/app/models';
 import { TitleStrategy } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-shows-entry',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <form [formGroup]="form" (ngSubmit)="add()">
+    <form [formGroup]="form" (ngSubmit)="add(foci)">
       <div>
         <label for="title">Title</label>
-        <input type="text" id="title" formControlName="title">
+        <input type="text" id="title" formControlName="title" autofocus #foci>
+        <div class="error-display" *ngIf="title.invalid && (title.touched || title.dirty)">
+          <p *ngIf="title.hasError('required')">The title is required.</p>
+          <p *ngIf="title.hasError('maxlength')">That is too long.</p>
+        </div>
       </div>
       <div>
         <label for="streamingPlatform">Streaming Platform</label>
         <select type="text" id="streamingPlatform" formControlName="streamingPlatform">
           <option *ngFor="let platform of platforms$ | async">{{platform}}</option>
         </select>
+        <div class="error-display" *ngIf="streamingPlatform.invalid && (streamingPlatform.touched || streamingPlatform.dirty)">
+          <p *ngIf="streamingPlatform.hasError('required')">Please select a streaming platform.</p>
+        </div>
       </div>
       <button type='submit'>Add New Show</button>
   </form>
   `,
   styles: [
+    ".error-display { color: red }" // why does THIS make my side by side flow break?
   ]
 })
 export class ShowsEntryComponent {
   private readonly service = inject(ShowsDataService);
-  readonly platforms$ = this.service.query(selectStreamingPlatforms);
-  form = new FormGroup({
-    title: new FormControl(''),
-    streamingPlatform: new FormControl('')
+  readonly platforms$ = this.service.query(selectStreamingPlatforms).pipe(map(platforms => ['', ...platforms]))
+  form = new FormGroup<FormDataType<ShowCreate>>({
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(100)]
+    }),
+    streamingPlatform: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    })
   })
 
-  add() {
-    console.log('adding', this.form.value);
-    const showCreate: ShowCreate = {
-      title: this.form.controls.title.value!,
-      streamingPlatform: this.form.controls.streamingPlatform.value!
+  get title() { return this.form.controls.title; }
+  get streamingPlatform() { return this.form.controls.streamingPlatform; }
+  add(foci: HTMLInputElement) {
+    if (this.form.valid) {
+      const showCreate: ShowCreate = {
+        title: this.form.controls.title.value!,
+        streamingPlatform: this.form.controls.streamingPlatform.value!
+      }
+      this.service.addShow(showCreate);
+      this.form.reset();
+      foci.focus();
     }
-    this.service.addShow(showCreate);
   }
 }
+
+type FormDataType<T> = {
+  [Property in keyof T]: FormControl<T[Property]>;
+};
